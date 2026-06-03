339d3192b4622f94a8b0e07d321adb9d9c8ab107
Yujie Tian <2025030055@mail.hfut.edu.cn>
2026-06-03 10:30:59 +0800

    Add files via upload

---
+++ eval_scripts/eval_fetaqa.py
@@ -0,0 +1,64 @@
+import argparse
+# import evaluate
+import json
+from rouge import Rouge
+import numpy as np
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+
+
+def compute_rouge(answers, targets):
+    rouger = Rouge()
+    rouge_1_f_scores = []
+    rouge_2_f_scores = []
+    rouge_l_f_scores = []
+
+    for idx, (answer, target) in enumerate(zip(answers, targets)):
+        try:
+            scores = rouger.get_scores(answer, target)[0]
+            rouge_1_f_scores.append(scores['rouge-1']['f'])
+            rouge_2_f_scores.append(scores['rouge-2']['f'])
+            rouge_l_f_scores.append(scores['rouge-l']['f'])
+        except ValueError as e:
+            print(f"Error at index {idx}: {e}")
+            print(f"Answer: {answer}")
+            print(f"Target: {target}")
+            continue
+
+    avg_rouge_1_f = sum(rouge_1_f_scores) / len(rouge_1_f_scores)
+    avg_rouge_2_f = sum(rouge_2_f_scores) / len(rouge_2_f_scores)
+    avg_rouge_l_f = sum(rouge_l_f_scores) / len(rouge_l_f_scores)
+
+    return {'rouge_1': avg_rouge_1_f,
+            'rouge_2': avg_rouge_2_f,
+            'rouge_l': avg_rouge_l_f}
+
+def compute_bleu(labels, preds, weights=None):
+    from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+    weights = weights or (0.25, 0.25, 0.25, 0.25)
+    return np.mean([sentence_bleu(references=[label],
+                                  hypothesis=pred,
+                                  smoothing_function=SmoothingFunction().method1,
+                                  weights=weights) for label, pred in zip(labels, preds)])
+
+def main(args):
+    data = []
+    with open(args.pred_file, "r") as f:
+        for line in f:
+            data.append(json.loads(line))
+
+    test_examples_answer = [x["output"] for x in data]
+    test_predictions_pred = [x["predict"].strip("</s>") for x in data]
+    predictions = test_predictions_pred
+    references = test_examples_answer
+
+    results = compute_rouge(answers=predictions, targets=references)
+    print(results)
+
+    results = compute_bleu(labels=predictions, preds=references)
+    print(results)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='arg parser')
+    parser.add_argument('--pred_file', type=str, default='/data/tyj/2D-TPE-main/res/fetaqa_adaptive_start_0p2_res.json', help='')
+    args = parser.parse_args()
+    main(args)
---
+++ eval_scripts/eval_hitab.py
@@ -0,0 +1,31 @@
+import json
+import sys
+import argparse
+from table_utils import evaluate
+
+
+def main(args):
+    data = []
+    with open(args.pred_file, "r") as f:
+        for line in f:
+            data.append(json.loads(line))
+
+    pred_list = []
+    gold_list = []
+    for i in range(len(data)):
+        if len(data[i]["predict"].strip("</s>").split(">, <")) > 1:
+            instance_pred_list = data[i]["predict"].strip("</s>").split(">, <")
+            pred_list.append(instance_pred_list)
+            gold_list.append(data[i]["output"].strip("</s>").split(">, <"))
+        else:
+            pred_list.append(data[i]["predict"].strip("</s>"))
+            gold_list.append(data[i]["output"].strip("</s>"))
+
+    print(evaluate(gold_list, pred_list))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='arg parser')
+    parser.add_argument('--pred_file', type=str, default='/data/tyj/2D-TPE-main/res/hitab_adaptive_start_0p2_res.json', help='')
+    args = parser.parse_args()
+    main(args)
